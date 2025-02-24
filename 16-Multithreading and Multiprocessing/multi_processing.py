8856354b5283518763a676ae2e30bcc8d21b96e0
Mohd Mudaseer Mazharuddin <mudaseer2753@gmail.com>
2025-02-24 23:20:43 +0530

    worked on threads

--- 16-Multithreading and Multiprocessing/multi_processing.py
+++ 16-Multithreading and Multiprocessing/multi_processing.py
@@ -3,33 +3,27 @@
 ## PArallel execution- Multiple cores of the CPU
 
 import multiprocessing
-
 import time
 
-def square_numbers():
+def square():
     for i in range(5):
         time.sleep(1)
-        print(f"Square: {i*i}")
-
-def cube_numbers():
+        print(f"square:{i*i}")
+def cube():
     for i in range(5):
-        time.sleep(1.5)
-        print(f"Cube: {i * i * i}")
+        time.sleep(1)
+        print(f"cube;{i*i*i}")
+if __name__=='__main__':
+    p1=multiprocessing.Process(target=square)
+    p2=multiprocessing.Process(target=cube)
 
-if __name__=="__main__":
-
-    ## create 2 processes
-    p1=multiprocessing.Process(target=square_numbers)
-    p2=multiprocessing.Process(target=cube_numbers)
-    t=time.time()
-
-    ## start the process
+    # start the process
     p1.start()
     p2.start()
+    t=time.time()
 
-    ## Wait for the process to complete
+    # wait for the processes to complete
     p1.join()
     p2.join()
-
     finished_time=time.time()-t
-    print(finished_time)
+    print(f"finished_time:{finished_time}")
